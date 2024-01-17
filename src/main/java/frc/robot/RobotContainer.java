f8fc87df7f72e17d54cbd121a5e2d31478532210
LocalStore <84473197+YourLocalStore@users.noreply.github.com>
2024-01-17 01:49:22 -0500

    configure binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,7 +12,6 @@ import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -24,13 +23,7 @@ public class RobotContainer {
 
   // The robot's subsystems and commands are defined here...
   private DriveSubsystem m_drive = new DriveSubsystem();
-
   private Command m_drivecommand = new DefaultDriveCommand(m_drive);
-  private Command m_deceleratecommand = new DecelerateDriveCommand(m_drive);
-
-  public DriveSubsystem VroomVroom(){
-    return m_drive;
-  }
   
   private final CommandXboxController m_driverController =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
@@ -45,26 +38,15 @@ public class RobotContainer {
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_drive::exampleCondition)
-        .onTrue(new DefaultDriveCommand(m_drive));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_drive.exampleMethodCommand());
+    m_driverController.leftBumper().whileTrue(new DecelerateDriveCommand(m_drive));
   }
 
-  /**
+  public DriveSubsystem VroomVroom(){
+    return m_drive;
+  }
+
+   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
@@ -73,4 +55,6 @@ public class RobotContainer {
     // An example command will be run in autonomous
     return Autos.exampleAuto(m_drive);
   }
+  
 }
+
