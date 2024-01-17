882c648268cbad4673d507fc54579c250c86e471
Austin <thisistotallyadiscordalt@gmail.com>
2024-01-17 14:08:28 -0500

    quick changes

--- src/main/java/frc/robot/commands/DecelerateDriveCommand.java
+++ src/main/java/frc/robot/commands/DecelerateDriveCommand.java
@@ -1,63 +1,63 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// // Copyright (c) FIRST and other WPILib contributors.
+// // Open Source Software; you can modify and/or share it under the terms of
+// // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+// package frc.robot.commands;
 
-import frc.robot.Input;
-import frc.robot.Constants;
-import frc.robot.subsystems.DriveSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
+// import frc.robot.Input;
+// import frc.robot.Constants;
+// import frc.robot.subsystems.DriveSubsystem;
+// import edu.wpi.first.wpilibj2.command.Command;
 
-public class DecelerateDriveCommand extends Command {
-  private final DriveSubsystem m_drive;
+// public class DecelerateDriveCommand extends Command {
+//   private final DriveSubsystem m_drive;
 
-  public DecelerateDriveCommand(DriveSubsystem subsystem) {
-    m_drive = subsystem;
-    addRequirements(subsystem);
-  }
+//   public DecelerateDriveCommand(DriveSubsystem subsystem) {
+//     m_drive = subsystem;
+//     addRequirements(subsystem);
+//   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+//   // Called when the command is initially scheduled.
+//   @Override
+//   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
 
-    // X, Y Movement
-    double throttle = Input.getHorizontal();
-    double turn = Input.getVerical(); 
+//     // X, Y Movement
+//     double throttle = Input.getHorizontal();
+//     double turn = Input.getVertical(); 
 
-    // For Deceleration
-    double speedReduction = Constants.SPEED_REDUCTION;
+//     // For Deceleration
+//     double speedReduction = Constants.SPEED_REDUCTION;
 
-    if (Input.driveDecelerate()){
-      speedReduction = Constants.SPEED_REDUCTION;
-      // DriveSubsystem.driveTrain.arcadeDrive(Input.getVertical() * speedReduction, Input.getHorizontal() * speedReduction);
-    }
+//     if (Input.driveDecelerate()){
+//       speedReduction = Constants.SPEED_REDUCTION;
+//       // DriveSubsystem.driveTrain.arcadeDrive(Input.getVertical() * speedReduction, Input.getHorizontal() * speedReduction);
+//     }
 
-    arcadeDrive(throttle, turn, speedReduction);
-  }
+//     arcadeDrive(throttle, turn, speedReduction);
+//   }
 
-  /*
-  * I don't know if this works I'm using a chromebook to code this
-  */ 
+//   /*
+//   * I don't know if this works I'm using a chromebook to code this
+//   */ 
 
-  public void arcadeDrive(double throttle, double turn, double speedReduction){
-    throttle *= speedReduction;
-    turn *= speedReduction;
+//   public void arcadeDrive(double throttle, double turn, double speedReduction){
+//     throttle *= speedReduction;
+//     turn *= speedReduction;
 
-    m_drive.arcadeDrive(throttle, turn);
-  }
+//     m_drive.arcadeDrive(throttle, turn);
+//   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
-}
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
